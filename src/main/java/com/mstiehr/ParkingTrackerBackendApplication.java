d93d137e0c866fd57ba11e720ce142938bea3bd5
Martin <git@maltin.pisces.uberspace.de>
2017-03-18 21:54:39 +0100

    initial commit

---
+++ src/main/java/com/mstiehr/ParkingTrackerBackendApplication.java
@@ -0,0 +1,12 @@
+package com.mstiehr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingTrackerBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mstiehr/ParkingTrackerBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mstiehr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParkingTrackerBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
